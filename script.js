8de4c1cad372869dc2fcce5afa26e5cb7591ab5d
Adam <19adamchristie96@gmail.com>
2022-09-14 08:15:46 +0100

    Create our function to get computer choice and test it. Also fix gameChoices variable so it contains strings.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const gameChoices = [rock, paper, scissors];
+const gameChoices = ["rock", "paper", "scissors"];
 let userChoice = prompt("Enter your choice (rock paper or scissors)");
+
+function getComputerChoice(array){
+    let index = Math.floor(Math.random() * 3);
+    return array[index];
+}
+
+getComputerChoice(gameChoices);
