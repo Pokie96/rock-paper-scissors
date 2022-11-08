04496b73a5d6734ea0f82ae836e37bd13e347551
Adam <19adamchristie96@gmail.com>
2022-11-08 20:55:21 +0000

    Add functionality to reset button so that the game is reset when pressed

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const paperBtn = document.getElementById('paper-button');
 const scissorsBtn = document.getElementById('scissors-button');
 const computerScoreDisplay = document.getElementById('computer-score');
 const userScoreDisplay = document.getElementById('user-score')
+const resetBtn = document.getElementById('reset-button');
+
+
+//Hide reset button by default
+resetBtn.style.display = 'none';
 
 
 //Button event listeners
@@ -25,6 +30,17 @@ paperBtn.addEventListener('click', function clickFunction(){
 scissorsBtn.addEventListener('click', function clickFunction(){
     playRound('scissors');
 });
+
+resetBtn.addEventListener('click', () => {
+    rockBtn.style.display = 'inline';
+    scissorsBtn.style.display = 'inline';
+    paperBtn.style.display = 'inline';
+    resetBtn.style.display = 'none';
+    userScore = 0;
+    computerScore = 0;
+    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+    userScoreDisplay.textContent = `User Score: ${userScore}`;
+})
 
 
 
@@ -60,7 +76,6 @@ function playRound(userChoice){
             computerScore += 1;
             userScore += 1;
         }
-
     } else if (userChoice === "scissors"){
         if (computerChoice === "paper"){
             alert(`You are the winner! ${userChoice} beats ${computerChoice}.`);
@@ -73,7 +88,6 @@ function playRound(userChoice){
             computerScore += 1;
             userScore += 1;
         }
-
     } else if (userChoice === "paper"){
         if (computerChoice === "rock"){
             alert(`You are the winner! ${userChoice} beats ${computerChoice}.`);
@@ -100,17 +114,20 @@ function checkWinner(){
         rockBtn.style.display = 'none';
         scissorsBtn.style.display = 'none';
         paperBtn.style.display = 'none';
+        resetBtn.style.display = 'inline';
         alert("It's a draw!");
     }
     else if(userScore === 5){
         rockBtn.style.display = 'none';
         scissorsBtn.style.display = 'none';
         paperBtn.style.display = 'none';
+        resetBtn.style.display = 'inline';
         alert('You are the winner!');
     }else if(computerScore === 5){
         rockBtn.style.display = 'none';
         scissorsBtn.style.display = 'none';
         paperBtn.style.display = 'none';
+        resetBtn.style.display = 'inline';
         alert('The computer beat you!')
     }
 }
